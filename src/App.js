50e8adcf4646910ce63d817a87afc25997ac99d6
Sinjiniroy <roysinjini15@gmail.com>
2023-12-08 23:32:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, {useState} from 'react';
 function App() {
+  const color = ['red', 'yellow', 'green', 'blue'];
+  const socket = new WebSocket('ws://localhost:8080');
+  const sendColorChange = (selectedColor) => {
+    if (socket.readyState === WebSocket.OPEN) {
+      socket.send(JSON.stringify({ type: 'colorChange', color: selectedColor }));
+    }
+  };
+  
+  const [currentColor, setCurrentColor] = useState(0);
+  const handleClicked = (index) => {
+    setCurrentColor(index);
+    sendColorChange(color[index]);
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Side 1</h1>
+      <div className="Btgrp">
+        <div className="Bt bt1" onClick={() => handleClicked(0)}></div>
+        <div className="Bt bt2" onClick={() => handleClicked(1)}></div>
+        <div className="Bt bt3" onClick={() => handleClicked(2)}></div>
+        <div className="Bt bt4" onClick={() => handleClicked(3)}></div>
+      </div>
+
+      <div className="main-box" style={{ backgroundColor: color[currentColor] }}>
+
+      </div>
+      
     </div>
   );
 }
 
 export default App;
+
